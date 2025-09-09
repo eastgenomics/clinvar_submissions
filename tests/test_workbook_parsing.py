f071ede4074e6655bec4f8a1fe03d7a0c2da93ac
RSWilson1 <robert.s.wilson1996@gmail.com>
2025-09-09 20:20:13 +0100

    formatting and fixed two tests using from class arguments

--- tests/test_workbook_parsing.py
+++ tests/test_workbook_parsing.py
@@ -1,20 +1,22 @@
 import glob
-import os
 import json
-from utils import utils
-import pandas as pd
-from pathlib import Path
+import os
 import unittest
-from openpyxl import load_workbook
-from freezegun import freeze_time
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
+from freezegun import freeze_time
+from openpyxl import load_workbook
+
+from utils import utils
 
 TEST_DATA_DIR = f"{Path(__file__).parent.resolve()}/test_data"
 
-nuh_workbooks = glob.glob(TEST_DATA_DIR + '/NUH/' + "*.xlsx")
-cuh_workbooks = glob.glob(TEST_DATA_DIR + '/CUH/' + "*.xlsx")
+nuh_workbooks = glob.glob(TEST_DATA_DIR + "/NUH/" + "*.xlsx")
+cuh_workbooks = glob.glob(TEST_DATA_DIR + "/CUH/" + "*.xlsx")
 
-with open(TEST_DATA_DIR + '/test_config.json') as f:
+with open(TEST_DATA_DIR + "/test_config.json") as f:
     config = json.load(f)
 
 
@@ -29,8 +31,12 @@ class TestParsing(unittest.TestCase):
         # Load 2 example cuh/nuh workbooks and dataframes for use in tests
         cls.cuh_workbook = load_workbook(cls.workbook_paths["cuh"])
         cls.nuh_workbook = load_workbook(cls.workbook_paths["nuh"])
-        cls.cuh_df_included = utils.get_included_fields(cls.cuh_workbook, cls.workbook_paths["cuh"])
-        cls.cuh_df_report, cls.cuh_msg = utils.get_report_fields(cls.cuh_workbook, config, cls.cuh_df_included)
+        cls.cuh_df_included = utils.get_included_fields(
+            cls.cuh_workbook, cls.workbook_paths["cuh"]
+        )
+        cls.cuh_df_report, cls.cuh_msg = utils.get_report_fields(
+            cls.cuh_workbook, config, cls.cuh_df_included
+        )
 
     def test_get_folder(self):
         """
@@ -98,12 +104,13 @@ class TestParsing(unittest.TestCase):
         with self.subTest("consequence extracted correctly"):
             assert list(df["consequence"]) == [
                 "intron_variant&splice_polypyrimidine_tract_variant&splice_region_variant",
-                "stop_gained"
+                "stop_gained",
             ]
 
         with self.subTest("HGVSc extracted correctly"):
             assert list(df["hgvsc"]) == [
-                "NM_000368.5:c.2626-5_2626-4dup", "NM_000548.5:c.4255C>T"
+                "NM_000368.5:c.2626-5_2626-4dup",
+                "NM_000548.5:c.4255C>T",
             ]
 
     def test_get_report_fields(self):
@@ -132,58 +139,58 @@ class TestParsing(unittest.TestCase):
                 "prevalence",
                 "hgvsc",
                 "germline_classification",
-                'pvs1',
-                'pvs1_evidence',
-                'ps1',
-                'ps1_evidence',
-                'ps2',
-                'ps2_evidence',
-                'ps3',
-                'ps3_evidence',
-                'ps4',
-                'ps4_evidence',
-                'pm1',
-                'pm1_evidence',
-                'pm2',
-                'pm2_evidence',
-                'pm3',
-                'pm3_evidence',
-                'pm4',
-                'pm4_evidence',
-                'pm5',
-                'pm5_evidence',
-                'pm6',
-                'pm6_evidence',
-                'pp1',
-                'pp1_evidence',
-                'pp2',
-                'pp2_evidence',
-                'pp3',
-                'pp3_evidence',
-                'pp4',
-                'pp4_evidence',
-                'bs1',
-                'bs1_evidence',
-                'bs2',
-                'bs2_evidence',
-                'bs3',
-                'bs3_evidence',
-                'ba1',
-                'ba1_evidence',
-                'bp2',
-                'bp2_evidence',
-                'bp3',
-                'bp3_evidence',
-                'bs4',
-                'bs4_evidence',
-                'bp1',
-                'bp1_evidence',
-                'bp4',
-                'bp4_evidence',
-                'bp5',
-                'bp5_evidence',
-                'bp7',
-                'bp7_evidence',
+                "pvs1",
+                "pvs1_evidence",
+                "ps1",
+                "ps1_evidence",
+                "ps2",
+                "ps2_evidence",
+                "ps3",
+                "ps3_evidence",
+                "ps4",
+                "ps4_evidence",
+                "pm1",
+                "pm1_evidence",
+                "pm2",
+                "pm2_evidence",
+                "pm3",
+                "pm3_evidence",
+                "pm4",
+                "pm4_evidence",
+                "pm5",
+                "pm5_evidence",
+                "pm6",
+                "pm6_evidence",
+                "pp1",
+                "pp1_evidence",
+                "pp2",
+                "pp2_evidence",
+                "pp3",
+                "pp3_evidence",
+                "pp4",
+                "pp4_evidence",
+                "bs1",
+                "bs1_evidence",
+                "bs2",
+                "bs2_evidence",
+                "bs3",
+                "bs3_evidence",
+                "ba1",
+                "ba1_evidence",
+                "bp2",
+                "bp2_evidence",
+                "bp3",
+                "bp3_evidence",
+                "bs4",
+                "bs4_evidence",
+                "bp1",
+                "bp1_evidence",
+                "bp4",
+                "bp4_evidence",
+                "bp5",
+                "bp5_evidence",
+                "bp7",
+                "bp7_evidence",
                 "comment_on_classification",
             ]
 
@@ -204,10 +211,10 @@ class TestParsing(unittest.TestCase):
 
         with self.subTest("pvs1 evidence extracted correctly"):
             assert df["pvs1_evidence"][0] == (
-                    "Exon present in all transcripts on gnomAD. "
-                    "LOF known mechanism of disease. "
-                    "Predicted to undergo nonsense-mediated decay."
-                )
+                "Exon present in all transcripts on gnomAD. "
+                "LOF known mechanism of disease. "
+                "Predicted to undergo nonsense-mediated decay."
+            )
 
         with self.subTest("ps4 evidence extracted correctly"):
             assert df["ps4_evidence"][0] == (
@@ -234,13 +241,11 @@ class TestParsing(unittest.TestCase):
             assert pd.isna(df["pp1_evidence"][0])
 
         with self.subTest("No error message should be returned"):
-            assert self.msg is None
+            assert self.cuh_msg is None
 
     def test_check_interpret_table_returns_no_error_if_hgvsc_matches(self):
         error_msg = utils.check_interpret_table(
-            self.cuh_df_report,
-            self.cuh_df_included,
-            config
+            self.cuh_df_report, self.cuh_df_included, config
         )
         assert error_msg is None
 
@@ -252,14 +257,9 @@ class TestParsing(unittest.TestCase):
         df_report, msg = utils.get_report_fields(
             wrong_hgvsc_workbook, config, df_include
         )
-        error_msg = utils.check_interpret_table(
-            df_report,
-            df_include,
-            config
-        )
+        error_msg = utils.check_interpret_table(df_report, df_include, config)
         assert error_msg == (
-            "HGVSc in interpret table does not match with that in included "
-            "sheet"
+            "HGVSc in interpret table does not match with that in included sheet"
         )
 
     def test_check_interpret_table_error_if_hgvsc_empty(self):
@@ -270,11 +270,7 @@ class TestParsing(unittest.TestCase):
         df_report, msg = utils.get_report_fields(
             empty_hgvsc_workbook, config, df_include
         )
-        error_msg = utils.check_interpret_table(
-            df_report,
-            df_include,
-            config
-        )
+        error_msg = utils.check_interpret_table(df_report, df_include, config)
         assert error_msg == "empty HGVSc in interpret table"
 
     def test_check_interpret_table_error_if_no_acmg_classification(self):
@@ -282,14 +278,8 @@ class TestParsing(unittest.TestCase):
         df_include = utils.get_included_fields(
             no_acmg_workbook, self.workbook_paths["cuh_empty_acmg"]
         )
-        df_report, msg = utils.get_report_fields(
-            no_acmg_workbook, config, df_include
-        )
-        error_msg = utils.check_interpret_table(
-            df_report,
-            df_include,
-            config
-        )
+        df_report, msg = utils.get_report_fields(no_acmg_workbook, config, df_include)
+        error_msg = utils.check_interpret_table(df_report, df_include, config)
         assert error_msg == "empty ACMG classification in interpret table"
 
     def test_check_interpret_table_error_if_wrong_acmg_classification(self):
@@ -300,11 +290,7 @@ class TestParsing(unittest.TestCase):
         df_report, msg = utils.get_report_fields(
             wrong_acmg_workbook, config, df_include
         )
-        error_msg = utils.check_interpret_table(
-            df_report,
-            df_include,
-            config
-        )
+        error_msg = utils.check_interpret_table(df_report, df_include, config)
         assert error_msg == "wrong ACMG classification in interpret table"
 
     def test_check_interpret_table_error_if_wrong_strength(self):
@@ -312,16 +298,13 @@ class TestParsing(unittest.TestCase):
             self.workbook_paths["nuh_wrong_interpret_strength"]
         )
         df_include = utils.get_included_fields(
-            wrong_interpret_strength_workbook, self.workbook_paths["nuh_wrong_interpret_strength"]
+            wrong_interpret_strength_workbook,
+            self.workbook_paths["nuh_wrong_interpret_strength"],
         )
         df_report, msg = utils.get_report_fields(
             wrong_interpret_strength_workbook, config, df_include
         )
-        error_msg = utils.check_interpret_table(
-            df_report,
-            df_include,
-            config
-        )
+        error_msg = utils.check_interpret_table(df_report, df_include, config)
         assert error_msg == "Wrong strength in pm2"
 
     def test_checking_sheet_wrong_summary(self):
@@ -330,7 +313,9 @@ class TestParsing(unittest.TestCase):
         assert msg == "extra col(s) added or change(s) done in summary sheet"
 
     def test_checking_sheet_wrong_interpret_row(self):
-        wrong_interpret_row = load_workbook(self.workbook_paths["nuh_wrong_interpret_row"])
+        wrong_interpret_row = load_workbook(
+            self.workbook_paths["nuh_wrong_interpret_row"]
+        )
         msg = utils.checking_sheets(wrong_interpret_row)
         assert msg == (
             "extra row(s) or col(s) added or change(s) done in interpret sheet"
@@ -365,43 +350,39 @@ class TestParsing(unittest.TestCase):
 
         with self.subTest("Vale for date_last_evaluated is date type"):
             assert isinstance(
-                df["date_last_evaluated"][0],
-                pd._libs.tslibs.timestamps.Timestamp
+                df["date_last_evaluated"][0], pd._libs.tslibs.timestamps.Timestamp
             )
-
 
     @freeze_time("2024-07-10 22:22:22")
     def test_no_evaluated_date(self):
-        '''
+        """
         Test that when there is no date last evaluated, today's date is used.
         Expect the time to change to 00:00 as we are only using date not time.
         This test uses an example workbook with nothing in the date cell
-        '''
+        """
         nuh_no_evaluated_date = self.workbook_paths["nuh_no_evaluated_date"]
         no_evaluated_date_workbook = load_workbook(nuh_no_evaluated_date)
         df, msg = utils.get_summary_fields(
             no_evaluated_date_workbook, config, nuh_no_evaluated_date
         )
-        assert str(df['date_last_evaluated'].item()) == "2024-07-10 00:00:00"
+        assert str(df["date_last_evaluated"].item()) == "2024-07-10 00:00:00"
 
     def test_error_message_if_date_last_evaluated_invalid(self):
-        '''
+        """
         Test that if the workbook has an evaluation date that is not compatible
         with datetime i.e. is not a date, the correct error message is returned
         which will cause this workbook to be skipped and added to parsing
         failed list.
         This test uses an example workbook with "Not valid" in the date cell
-        '''
+        """
         invalid_evaluation_date_workbook = load_workbook(
             self.workbook_paths["nuh_invalid_evaluated_date"]
         )
         df, msg = utils.get_summary_fields(
-            invalid_evaluation_date_workbook,
-            config,
-            self.workbook_paths["nuh_invalid_evaluated_date"]
+            invalid_evaluation_date_workbook, config, "NUH"
         )
         assert msg == (
-            "Value for date last evaluated \"Not valid\" is not compatible "
+            'Value for date last evaluated "Not valid" is not compatible '
             "with datetime conversion"
         )
 
@@ -410,22 +391,20 @@ class TestParsing(unittest.TestCase):
         Test if interpreted col (yes/no) is correctly filled in no error is
         raised.
         """
-        df_summary, _ = utils.get_summary_fields(
-            self.cuh_workbook, config, self.workbook_paths["cuh"]
-        )
+        df_summary, _ = utils.get_summary_fields(self.cuh_workbook, config, "CUH")
         df_merged = pd.merge(self.cuh_df_included, df_summary, how="cross")
-        df_final = pd.merge(df_merged, self.df_report, on="hgvsc", how="left")
+        df_final = pd.merge(df_merged, self.cuh_df_report, on="hgvsc", how="left")
         msg = utils.check_interpreted_col(df_final)
         assert msg is None
 
     def test_interpreted_col_errors_if_incongruous(self):
-        '''
+        """
         Test that there is a classification for each variant that has been
         interpreted and no classification if not interpreted.
-        '''
+        """
         data = [
-            ['no', 'Pathogenic', 'NM_000368.5:c.2626-5_2626-4dup'],
-            ['yes', np.nan, 'NM_000548.5:c.4255C>T']
+            ["no", "Pathogenic", "NM_000368.5:c.2626-5_2626-4dup"],
+            ["yes", np.nan, "NM_000548.5:c.4255C>T"],
         ]
         df = pd.DataFrame(
             data, columns=["interpreted", "germline_classification", "hgvsc"]
@@ -441,8 +420,8 @@ class TestParsing(unittest.TestCase):
 
     def test_interpreted_col_errors_if_value_neither_yes_or_no(self):
         data = [
-            ['yes', 'Pathogenic', 'NM_000368.5:c.2626-5_2626-4dup'],
-            ['foo', np.nan, 'NM_000548.5:c.4255C>T']
+            ["yes", "Pathogenic", "NM_000368.5:c.2626-5_2626-4dup"],
+            ["foo", np.nan, "NM_000548.5:c.4255C>T"],
         ]
         df = pd.DataFrame(
             data, columns=["interpreted", "germline_classification", "hgvsc"]
