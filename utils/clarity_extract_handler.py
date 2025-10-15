e2a28a65cd18198bbe67fcf546187cfec16f950f
RSWilson1 <robert.s.wilson1996@gmail.com>
2025-10-15 16:08:51 +0100

    Split the handle_clarity_extract function into component parts and moved nested functions to normal functions. Changed regex to work with or without genome_build in the run name. Changed assay search to reflect files as TWE isn't present. Combined filtering CNV/mosaics to single cmd

--- utils/clarity_extract_handler.py
+++ utils/clarity_extract_handler.py
@@ -10,6 +10,7 @@ from pandas import DataFrame
 from pathlib import Path
 import os
 
+
 def open_files(clarity):
     """
     Open files and read in file contents to DataFrames
@@ -213,7 +214,9 @@ def create_path(filename, base_path, assay, run):
     base_path = Path(base_path)
 
     # extract run_name from run string
-    run_name_match = re.search(r"(002_)(\d{6}_[A-Z0-9]+_\d{4}_[A-Z0-9]+)_(\d{2})?_[A-Za-z0-9]+", run)
+    run_name_match = re.search(
+        r"(002_)(\d{6}_[A-Z0-9]+_\d{4}_[A-Z0-9]+)(_\d{2})?_[A-Za-z0-9]+", run
+    )
     # group 1 = 002_
     # group 2 = DDMMYY_SEQUENCER_TESTCODE_FLOWCELL
     # group 3 = genome build
@@ -222,7 +225,9 @@ def create_path(filename, base_path, assay, run):
 
     # Return None if run_name extraction failed
     if run_name is None:
-        print(f"Warning: Could not extract run_name from '{run}' for filename {filename}. Returning None.")
+        print(
+            f"Warning: Could not extract run_name from '{run}' for filename {filename}. Returning None."
+        )
         return None
 
     # Build path depending on assay
@@ -235,7 +240,6 @@ def create_path(filename, base_path, assay, run):
             f"Warning: Unknown assay '{assay}' for filename {filename}. Returning None."
         )
         return None
-
     return path
 
 
@@ -249,9 +253,9 @@ def find_file_name(search_query):
         filename (str): a file name, or None if no or multiple matches found
     """
     files = list(
-        dxpy.find_data_objects(name=search_query, name_mode="glob", describe={
-            "fields": {"name": True}
-        })
+        dxpy.find_data_objects(
+            name=search_query, name_mode="glob", describe={"fields": {"name": True}}
+        )
     )
     filenames = [file.get("describe").get("name") for file in files]
     if len(filenames) != 1:
@@ -261,87 +265,89 @@ def find_file_name(search_query):
         return filenames[0]
 
 
-def handle_clarity_extract(
-    clarity_extract_path, assays, base_path
-) -> tuple[DataFrame, DataFrame, DataFrame]:
+def is_report_code_in_list(row) -> bool:
     """
-    Main function to handle clarity extract and return paths to workbooks
+    Check if report_r_code is in R_codes list
+
     Inputs:
-
-        clarity_extract_path (str): path to clarity extract file
-        assays (list): list of assays to process, e.g. ['CEN', 'TWE']
-        base_path (str): base path to clingen folder
+        row (pd.Series): a row from the DataFrame containing 'R_codes' and 'report_r_code'
     Outputs:
-        report_df_filtered (pd.DataFrame): DataFrame of workbooks to process
-        missing_data_df (pd.DataFrame): DataFrame of workbooks with missing data
-        multiple_reports_df (pd.DataFrame): DataFrame of workbooks
-            with multiple workbooks per assay
+        bool: True if report_r_code is in R_codes, False otherwise
     """
-    # Read data into dataframes
-    clarity_df = open_files(clarity_extract_path)
+    r_codes = row["R_codes"]
+    report_code = row["report_r_code"]
+    if pd.isna(report_code) or pd.isna(r_codes):
+        return False
+    # Remove decimal from report_code for comparison
+    report_code_no_decimal = re.sub(r"\.\d+", "", report_code)
+    bool = report_code_no_decimal in [re.sub(r"\.\d+", "", code) for code in r_codes]
+    return bool
 
-    # Process data to construct a path for each specimen
-    # create df with column by splitting the Beaker Procedure Name to create a new column for assay
-    clarity_df["sample_id"] = clarity_df["Specimen Identifier"].str.split("-").str[1]
-    report_df = pd.DataFrame()
 
-    print(f"Processing assays: {assays}")
-    report_df = fetch_all_reports(clarity_df, assays)
+def extract_assay_from_filename(filename):
+    """
+    Extract the assay type from the given filename.
 
-    print(f"Total reports fetched: {report_df.shape[0]}")
+    Parameters
+    ----------
+    filename : str
+        The name of the file from which to extract the assay type.
 
-    # Split R codes into a list
-    report_df["R_codes"] = report_df["Test Directory Test Code"].fillna("").str.split("|")
-    # remove decimal points from R codes
-    report_df["R_codes"] = report_df["R_codes"].apply(
-        lambda x: [re.sub(r"\.\d+", "", code) for code in x if isinstance(code, str) and code.startswith("R")]
-    )
+    Returns
+    -------
+    str or None
+        The extracted assay type (CEN or WES) or None if not found.
+    """
+    if pd.isna(filename):
+        return None
+    match = re.search(r"(CEN|WES)", filename)
+    return match.group(1) if match else None
 
-    # Get Assay from file_name using regex
-    def extract_assay_from_filename(filename):
-        if pd.isna(filename):
-            return None
-        match = re.search(r"(CEN|WES|TWE)", filename)
-        return match.group(1) if match else None
 
-    report_df["Assay"] = report_df["file_name"].apply(extract_assay_from_filename)
+def filtering_reports(report_df):
+    """
+    Filter out reports based on if filename doesn't match the verified R codes
+    and if filename contains _CNV_ or _mosaic_
 
-    # Add the path to the processed reports
-    report_df["path"] = report_df.apply(
-        lambda x: create_path(x["file_name"], base_path, x["Assay"], x["project_name"]),
-        axis=1,
-    )
+    Inputs:
+        report_df (pd.DataFrame):
+            DataFrame containing report information with a 'file_name' column
+            and 'R_codes' column.
 
-    # Add specimen ID to the processed report_df
-    report_df["instrument_id"] = report_df["file_name"].str.split("-").str[0]
-    report_df["full_sample_id"] = (
-        report_df["instrument_id"] + "-" + report_df["sample_id"]
-    )
-    # create report_r_code column from file_name
-    report_df["report_r_code"] = report_df["file_name"].str.extract(r"_(R\d+\.\d+)_")[0]
+    Outputs:
+        filtered_df (pd.DataFrame): Filtered DataFrame excluding rows with '_CNV_' or '_mosaic_' in 'file_name'.
+    """
+    if report_df.empty or report_df is None:
+        print("Report DataFrame is empty or None. Skipping filtering.")
+        return report_df
 
-    # Check if report_r_code is in R_codes list
-    def is_report_code_in_list(row):
-        r_codes = row["R_codes"]
-        report_code = row["report_r_code"]
-        if pd.isna(report_code) or pd.isna(r_codes):
-            return False
-        # Remove decimal from report_code for comparison
-        report_code_no_decimal = re.sub(r"\.\d+", "", report_code)
-        return report_code_no_decimal in [re.sub(r"\.\d+", "", code) for code in r_codes]
-
+    report_df.to_csv("test_all_reports_df.csv", index=False)
     report_df["rcode_match"] = report_df.apply(is_report_code_in_list, axis=1)
-    # Filter rows where R code matches
+    # Filter rows where R code doesn't match
     report_df = report_df[report_df["rcode_match"]].copy()
+    # clean up by dropping the rcode_match column
     report_df = report_df.drop(columns=["rcode_match"])
+    # Filter out rows where filename contains _CNV_ or _mosaic_
+    filtered_df = report_df[
+        ~report_df["file_name"].str.contains(r"_(CNV|mosaic)_", na=False)
+    ]
+    filtered_df.to_csv("test_filtered_reports.csv", index=False)
 
-    # Stratify into multiple files for different outcomes
-    # Filter out all rows where filename contains _CNV_ or _mosaic_
-    report_df = report_df[~report_df["file_name"].str.contains("_CNV_", na=False)]
-    report_df = report_df[~report_df["file_name"].str.contains("_mosaic_", na=False)]
-    print(
-        f"After filtering out CNV and mosaic files: {report_df.shape[0]} rows remaining"
-    )
+    return filtered_df
+
+
+def remove_missing_and_multiple_reports(report_df):
+    """
+    Remove reports with missing or multiple files.
+
+    Inputs:
+        report_df (pd.DataFrame): DataFrame containing report information with a 'file_name' column.
+
+    Outputs:
+        report_df_filtered (pd.DataFrame): Filtered DataFrame excluding rows with missing or multiple files.
+        missing_data_df (pd.DataFrame): DataFrame containing rows with missing data in 'file_name' or 'R_codes'.
+        multiple_reports_df (pd.DataFrame): DataFrame containing rows with multiple reports per assay.
+    """
 
     missing_data = report_df["file_name"].isna() | report_df["R_codes"].isna()
     missing_data_df = report_df[missing_data]
@@ -395,4 +401,113 @@ def handle_clarity_extract(
         on=["sample_id", "Assay", "report_r_code"],
         how="inner",
     ).copy()
+    return report_df_filtered, missing_data_df, multiple_reports_df
+
+
+def preprocess_report_df(report_df, base_path):
+    """
+    Preprocess the report DataFrame by adding necessary columns and cleaning data.
+
+    Inputs:
+        report_df (pd.DataFrame): DataFrame containing report information with a 'file_name' column
+            and 'R_codes' column.
+        base_path (str or Path): Base path to the clingen folder.
+    Outputs:
+        report_df (pd.DataFrame): Preprocessed DataFrame with additional columns.
+    """
+    # Split R codes into a list
+    report_df["R_codes"] = (
+        report_df["Test Directory Test Code"].fillna("").str.split("|")
+    )
+
+    # remove decimal points from R codes
+    report_df["R_codes"] = report_df["R_codes"].apply(
+        lambda x: [
+            re.sub(r"\.\d+", "", code)
+            for code in x
+            if isinstance(code, str) and code.startswith("R")
+        ]
+    )
+
+    # Extract assay from filename
+    report_df["Assay"] = report_df["file_name"].apply(extract_assay_from_filename)
+
+    # Add the path to the processed reports
+    report_df["path"] = report_df.apply(
+        lambda x: create_path(x["file_name"], base_path, x["Assay"], x["project_name"]),
+        axis=1,
+    )
+
+    # Add specimen ID to the processed report_df
+    report_df["instrument_id"] = report_df["file_name"].str.split("-").str[0]
+    report_df["full_sample_id"] = (
+        report_df["instrument_id"] + "-" + report_df["sample_id"]
+    )
+    # create report_r_code column from file_name
+    report_df["report_r_code"] = report_df["file_name"].str.extract(r"_(R\d+\.\d+)_")[0]
+
+    return report_df
+
+
+def handle_clarity_extract(
+    clarity_extract_path, assays, base_path
+) -> tuple[DataFrame, DataFrame, DataFrame]:
+    """
+    Main function to handle clarity extract and return paths to workbooks
+    Inputs:
+
+        clarity_extract_path (str): path to clarity extract file
+        assays (list): list of assays to process, e.g. ['CEN', 'TWE']
+        base_path (str): base path to clingen folder
+    Outputs:
+        report_df_filtered (pd.DataFrame): DataFrame of workbooks to process
+        missing_data_df (pd.DataFrame): DataFrame of workbooks with missing data
+        multiple_reports_df (pd.DataFrame): DataFrame of workbooks
+            with multiple workbooks per assay
+    """
+    # Read data into dataframes
+    clarity_df = open_files(clarity_extract_path)
+
+    # Process data to construct a path for each specimen
+    # create df with column by splitting the Beaker Procedure Name to create a new column for assay
+    clarity_df["sample_id"] = clarity_df["Specimen Identifier"].str.split("-").str[1]
+    report_df = pd.DataFrame()
+
+    print(f"Processing assays: {assays}")
+    report_df = fetch_all_reports(clarity_df, assays)
+
+    # Add check for empty DataFrame
+    if report_df.empty:
+        print(
+            "No reports found after fetching from DNAnexus. Returning empty DataFrames."
+        )
+        empty_cols = {
+            "file_name": pd.NA,
+            "project_id": pd.NA,
+            "project_name": pd.NA,
+            "R_codes": pd.NA,
+            "Assay": pd.NA,
+            "path": pd.NA,
+            "instrument_id": pd.NA,
+            "full_sample_id": pd.NA,
+            "report_r_code": pd.NA,
+        }
+        empty_df = pd.DataFrame(
+            columns=list(clarity_df.columns) + list(empty_cols.keys())
+        )
+        return empty_df, empty_df.copy(), empty_df.copy()
+
+    print(f"Total reports fetched: {report_df.shape[0]}")
+
+    # Preprocess report_df
+    report_df = preprocess_report_df(report_df, base_path)
+    print(f"Report DataFrame after preprocessing: {report_df.shape[0]} rows")
+    # Filter out all rows where filename contains _CNV_ or _mosaic_
+    report_df = filtering_reports(report_df)
+
+    # Remove missing and multiple reports
+    report_df_filtered, missing_data_df, multiple_reports_df = (
+        remove_missing_and_multiple_reports(report_df)
+    )
+
     return report_df_filtered, missing_data_df, multiple_reports_df
