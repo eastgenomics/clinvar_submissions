f68be822f63b9f4caaad7d3130f0e55f6f329538
RSWilson1 <robert.s.wilson1996@gmail.com>
2025-11-07 12:35:32 +0000

    small change to print order

--- pandora.py
+++ pandora.py
@@ -235,10 +235,11 @@ def main():
         exclude = config["exclude"]
         # Add DUP to the accession_id field for duplicates to exclude them
         # from submission
-        print(
+
+        if not args.dry_run:
+            print(
             "Checking for duplicate variants... and setting accession_id to 'DUP'"
             )
-        if not args.dry_run:
             db.set_DUP_for_germline_duplicates(engine)
         else:
             print("Dry run specified. No changes will be made to the database.")
